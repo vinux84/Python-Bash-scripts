8721719d32f451021cc57771c7d2655569572c3a
Vincent <vincentb0824@gmail.com>
2023-10-10 16:12:46 -0700

    Update node_config.sh

--- node_config.sh
+++ node_config.sh
@@ -1,3 +1,6 @@
+# this requires that you have a linux machine and have set up multiple interfaces for the default subnet of the node, and the new subnet of the intended IP address for node. and a micro usb plugged into the configuration
+# Linux Machine
+
 #!/bin/sh
 
 clear 
@@ -5,17 +8,17 @@ echo "\n\n######################################################################
       \n\n			                       NODE CONFIGURATION
       \n\n################################################################################################################
 
-      \n\nRemember the node must be in Recovery Mode before any configuration can happen. 
-This can be done by switching the tab toward the arrow while the node is powered down, located next to the OTG port.
+      \n\nThe node must be in recovery mode before any configurations can happen. 
+This can be done by switching the tab in the same direction of the arrow while the node is powered down, it is located next to the OTG port.
 
-      \n\nNext, Plug a micro-usb from the OTG port into the Linux machine. Then power the device up using a 12v power supply. 
+      \n\nNext, Plug in a micro-usb in the OTG port on the node. Then power the device up using a 12v power supply. 
       \n\n"
 
 while true; do
     read -p "Are you ready to configure the node and begin the flash process? : " conf_input
     case $conf_input in
         [Yy]* ) echo "\nContinuing to flash"; break;;
-        [Nn]* ) echo "\nExiting process"; exit;;
+        [Nn]* ) echo "\nExiting process"; exit 1;;
         * ) echo "Please answer yes or no.";;
     esac
 done
@@ -29,7 +32,7 @@ do
         x=$(( $x + 1 ))
 done
 
-LSUSB_CHECK=$(lsusb | grep 'Bus 002 Device 031: ID 0955:7c18 NVidia Corp.') 
+LSUSB_CHECK=$(lsusb | grep 'NVIDIA') 
 
 if [ -z "$LSUSB_CHECK" ]; then
 	sleep 2
@@ -46,13 +49,6 @@ if cd ~/Desktop/Node_Configuration/Firmware/v0.1/Linux_for_Tegra && sudo ./flash
 	sleep 2						       
 	echo -n "\n\nFlash completed successfully!"
 	sleep 4
-	x=1
-	while [ $x -le 5 ] 
-	do
-        	echo -n "!"
-        	sleep .2
-        	x=$(( $x + 1 ))
-	done
 else
 	echo "\n\nFlash was not successful, exiting now"
 	sleep 2
@@ -60,113 +56,124 @@ else
 fi
 
 clear
+
 echo "\n\n###############################################################################################################
       \nPower down the node and take the node out of recovery mode
       \nUnplug the micro-usb from the OTG port
-      \nPlug an ethernet cable from the Linux machine into port 1
-
-      \n######\n\nTIP: the next part is configuring the node and assigning an IP 
-while you do the next part, if you need to flash several nodes
-you are able to start another 25 minute flash sequence on 
+      \nPlug an ethernet cable from the configuration into port 1
+      \n######\n\nThe next part is configuring the node and assigning a IP to it.
+While you do the next part, if you need to flash another node
+you can start another 25 minute flash sequence on
 a second node while configuring the first.
       \n######
-
       \nLet's confirm the node works by pinging it, then assigning it a ip address\n\n"
-
+      
 while true; do
-    read -p "Have you completed the above instructions and are you ready to change this computers ip address to ping node? : " ip_input
+    read -p "Have you completed the above instructions and are you ready to go forward with configuring the node? : " ip_input
     case $ip_input in
-        [Yy]* ) echo "\nChanging PC's ip address to the node's subnet"; break;;
-        [Nn]* ) echo "\nPlease finish the above instructions to ping node and assign it a ip address";;
+        [Yy]* ) echo "\nConfiguring the Node"; break;;
+        [Nn]* ) echo "\nPlease finish the above instructions to continue the configuration process on the node";;
         * ) echo "Please answer yes or no.";;
     esac
 done
 
-sudo ifconfig wlp0s20f3 192.168.8.25 netmask 255.255.255.0      # this code just needs to change to the Eth. interface name on linux machine
-
-IP_CHECK=$(ifconfig | grep 'inet 192.168.8.25  netmask 255.255.255.0  broadcast 192.168.8.255')  
-
-if [ -z "$IP_CHECK" ]; then
-       	sleep 2
-        echo "\n\nIP address did not change"
+if ping -c 4 192.168.8.101; then
         sleep 2
-	exit
-else
-	sleep 2
-	echo "\n\nConfiguration machine IP changed, pinging node to confirm...."
-        sleep 2
-fi
-
-if ping -c 4 192.168.8.101; then                  
-        sleep 2                                                
-        echo -n "\nPing completed successfully!"
+        echo -n "\nNode is up, default ping completed successfully and can continue the process"
         x=1
-        while [ $x -le 5 ] 
+        while [ $x -le 5 ]
         do
                 echo -n "!"
                 sleep .2
                 x=$(( $x + 1 ))
         done
 else
-        echo "\nPing was not successful, exiting now"
+        echo "\nDefault ping was not successful, check your configuration machine interfaces. exiting now"
         sleep 2
         exit
 fi
 
-sudo systemctl stop NetworkManager && sudo systemctl enable NetworkManager && sudo systemctl start NetworkManager
+if echo "nvidia" | sudo -S ssh-copy-id -i ~/Desktop/Node_Configuration/ssh_key nvidia@192.168.8.101 ; then      # I added "sudo -S" to the command. have not tried -  the password pipe didn't work. had to manually put in pw. maybe a expect block would work better.
+        sleep 2
+        echo -n "\nSSH key sent to node.."
+        sleep 4
+else
+        echo "\nSSH key was not sent to node, something is wrong. exiting now"
+        sleep 2
+        exit 1
+fi
 
-echo "\n\nNext you will enter the production ip address so it can be configured to the Node as well as the routers ip address"
+echo "\nNext you will enter the production ip address and gateway ip address to the node"
 
+read -p "\nPlease put in production ip address for node : " prod_ip_node
+read -p "\nPlease put in production ip address for node's gateway : " prod_ip_router
 
-read -p "\n\nPlease put in production ip address for node : " prod_ip_node
-read -p "\n\nPlease put in production ip address for node's router : " prod_ip_router
-
-echo "\n\nChanging IP address to production IP address to node and assigning its gateway ip address"
+echo "\nChanging default ip address to production ip address and assigning its gateway ip address"
 sleep 2
 
-ssh-copy-id -i ~/Desktop/Node_Configuration/ssh_key nvidia@192.168.8.101
-
-bash /home/cainthus/Desktop/Node_Configuration/change_ip.sh ~/Desktop/Node_Configuration/ssh_key $prod_ip_node/24 255.255.255.0 $prod_ip_router
-
-
-
-# Finally, validate the new IP address by changing this computer to be under the same subnet, and try pinging the new IP address:
-
-# need to figure out how to open another terminal to ping node
-
-sudo ifconfig wlp0s20f3 10.0.1.101 netmask 255.255.255.0      # this code just needs to change to the Eth. interface name on linux machine
-							      # Also need to split up prod_ip_node, so like 10.0.x.25 (ip address) to assign pc in same subnet
-IP_CHECK=$(ifconfig | grep 'inet 10.0.1.101  netmask 255.255.255.0  broadcast 192.168.8.255')
-
-if [ -z "$IP_CHECK" ]; then
+if bash /home/cainthus/Desktop/Node_Configuration/change_ip.sh ~/Desktop/Node_Configuration/ssh_key $prod_ip_node/24 255.255.255.0 $prod_ip_router ; then
         sleep 2
-        echo "\n\nIP address did not change"
-        sleep 2
-        exit
+        echo -n "\nNode IP change was not successful"
+        sleep 4
+        exit 1
 else
-        sleep 2
-        echo "\n\nLinux machine IP changed continuing to ping"
+        echo "\nNode IP change was successful"
         sleep 2
 fi
+
+echo "\nWaiting for node to reboot..."
+sleep 45
+echo "\nConfirming node ip address has changed..."
 
 if ping -c 4 $prod_ip_node; then
         sleep 2
-        echo -n "\nPing completed successfully!"
+        echo -n "\nSuccessfully pinged new ip address of node"
         x=1
-        while [ $x -le 5 ] 
+        while [ $x -le 5 ]
         do
                 echo -n "!"
                 sleep .2
                 x=$(( $x + 1 ))
         done
 else
-        echo "\nPing was not successful, exiting now"
+        echo "\nPing was not successful, node IP didn't change or something is wrong with your interfaces. exiting now"
         sleep 2
-        exit
+        exit 1
 fi
 
+echo "\nFormatting SSD now, this will take a few minutes...."
+sleep 2
+# below will ask for a yes, as its the first time sshing in.
+ssh -i ~/Desktop/Node_Configuration/ssh_key nvidia@$prod_ip_node "bash -s" << EOF
+	sudo wipefs -a /dev/sda
+	sudo parted /dev/sda mklabel gpt
+	sudo parted /dev/sda mkpart primary ext4 0% 100%
+	sudo mkfs.ext4 /dev/sda1
+	sudo mount /dev/sda1 /mnt
+	sudo su
+	cp -ax / /mnt && sync
+ 	exit
+  	sudo cp /boot/Image /boot/Image.backup
+EOF
 
-# ping 10.0.x.101
+echo "\nSending boot settings to node..."
+sleep 2
+
+scp -i ~/Desktop/Node_Configuration/ssh_key /home/cainthus/Desktop/Node_Configuration/boot_settings nvidia@$prod_ip_node:~/
+
+echo "\nBacking up boot settings and rebooting..."
+sleep 2
+
+ssh -i ~/Desktop/Node_Configuration/ssh_key nvidia@$prod_ip_node "bash -s" << EOF
+	sudo cp /boot/extlinux/extlinux.conf /boot/extlinux/extlinux.conf.backup
+ 	sudo mv boot_settings /boot/extlinux/extlinux.conf
+  	sudo reboot
+EOF
+
+echo "\nThat's it. ALL DONE!!"
+
+exit 0
+
 
 
 
